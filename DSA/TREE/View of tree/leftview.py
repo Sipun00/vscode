127d237a90e9cc8d3fe653773fd9a5c69c687767
Mohit <mohitsamal69@gmail.com>
2022-04-03 10:48:04 +0530

    commit

---
+++ DSA/TREE/View of tree/leftview.py
@@ -0,0 +1,133 @@
+"""Left View of Binary Tree 
+Easy Accuracy: 37.86% Submissions: 100k+ Points: 2
+Given a Binary Tree, print Left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from Left side. The task is to complete the function leftView(), which accepts root of the tree as argument.
+
+Left view of following tree is 1 2 4 8.
+
+          1
+       /     \
+     2        3
+   /     \    /    \
+  4     5   6    7
+   \
+     8   
+
+Example 1:
+
+Input:
+   1
+ /  \
+3    2
+Output: 1 3
+
+Example 2:
+
+Input:
+
+Output: 10 20 40
+"""
+
+'''
+# Node Class:
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+#Function to return a list containing elements of left view of the binary tree.
+def solve(root,ans,level):
+    if root==None:
+        return
+    if level==len(ans):
+        ans.append(root.data)
+    solve(root.left,ans,level+1)
+    solve(root.right,ans,level+1)
+def LeftView(root):
+    
+    # code here
+    ans=[]
+    solve(root,ans,0)
+    return ans
+
+#{ 
+#  Driver Code Starts
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+   
+    
+        s=input()
+        root=buildTree(s)
+        result = LeftView(root)
+        for value in result:
+            print(value,end=" ")
+        print()
+
+# } Driver Code Ends
--- PYTHON CODE/helpfriend/soumya.py
+++ PYTHON CODE/helpfriend/soumya.py
@@ -1,23 +1,23 @@
-c=input()
-V=list(map(int,c.split()))
-N=V[0]
-
-K=V[1]
-b=input()
-A=list(map(int,b.split()))
-A=set(A)
-c=0
-for i in A:
-    if i-2 in A or i+2 in A :
-        c+=1
-    elif i-2>= min(A) and i+2<=max(A) :
-        for j in range (i-2,i+3):
-            if j in A:
-                c+=1
-                break
-print(c)
-
-
+# c=input()
+# V=list(map(int,c.split()))
+# N=V[0]
+# 
+# K=V[1]
+# b=input()
+# A=list(map(int,b.split()))
+# A=set(A)
+# c=0
+# for i in A:
+    # if i-2 in A or i+2 in A :
+        # c+=1
+    # elif i-2>= min(A) and i+2<=max(A) :
+        # for j in range (i-2,i+3):
+            # if j in A:
+                # c+=1
+                # break
+# print(c)
+# 
+# 
 
 
 
@@ -36,3 +36,50 @@ print(c)
         # if i<=K:
             # c+=1
     # print(c)
+
+
+
+
+
+def sum(cls,input1,input2):
+    l=max(cls.input2)+min(cls.input2)
+    return l
+    pass
+
+
+def lis(cls,input1,input2):
+
+    k=[1]*cls.input1
+    for i in range(1,cls.input1):
+        
+        for j in range(0,i):
+            if cls.input2[i]>cls.input2[j] and k[i]<k[j]+1:
+                k[i]=k[j]+1
+
+    
+
+
+    maxi=0
+    for i in range(cls.input1):
+        maxi=max(maxi,k[i])
+    return maxi
+    pass
+
+
+input1=5
+input2=[41,18467,6334,26500,19169]
+print(lis(input1,input2))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
